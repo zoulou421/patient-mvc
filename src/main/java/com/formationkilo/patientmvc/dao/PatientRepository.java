824c7a6a1d7d484aeb2b7f4dd3d7854460d67719
Bonevy BEBY <bonevybeby@gmail.com>
2023-11-05 13:26:49 +0100

    add search form//adapt pagination with/add delete method and//Concept of client and server approach

--- src/main/java/com/formationkilo/patientmvc/dao/PatientRepository.java
+++ src/main/java/com/formationkilo/patientmvc/dao/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.formationkilo.patientmvc.dao;
 
 import com.formationkilo.patientmvc.dto.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository  extends JpaRepository<Patient,Long> {
+    Page<Patient>findByNomContains(String kw, Pageable pageable);
 }
--- src/main/java/com/formationkilo/patientmvc/ui/PatientController.java
+++ src/main/java/com/formationkilo/patientmvc/ui/PatientController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -22,7 +23,9 @@ public class PatientController {
 
     @GetMapping(path = "/")
     public String patients(){
-        return "patients";
+
+        //return "patients";
+        return "redirect:/index";
     }
 
   @GetMapping(path = "/home")
@@ -35,13 +38,27 @@ public class PatientController {
   @GetMapping(path = "/index")
   public String paginationPatients(Model model,
                                    @RequestParam(name="page", defaultValue = "0") int page,
-                                   @RequestParam(name="size", defaultValue = "5") int size){
-      Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+                                   @RequestParam(name="size", defaultValue = "5") int size,
+                                   @RequestParam(name="keyword", defaultValue = "") String keyword ){
+    //  Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+      Page<Patient> pagePatients = patientRepository.findByNomContains(keyword,PageRequest.of(page,size));
       model.addAttribute("pagePatients",pagePatients.getContent());
      //total number of pages
       model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
       model.addAttribute("currentPage", page);
+      model.addAttribute("keyword", keyword);
       return "patients";
   }
 
+    @GetMapping(path = "/delete")
+  public String delete(Long id, int page, String keyword){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+  }
+
+    @GetMapping(path = "/patients")
+    @ResponseBody
+  public  List<Patient> listPatients(){
+        return patientRepository.findAll();
+  }
 }

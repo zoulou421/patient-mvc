6052ca57dbeb934932e1b55a1fa5d649de7e1f6e
Bonevy BEBY <bonevybeby@gmail.com>
2023-11-05 10:43:54 +0100

    Add pagination for Patients//mysql db used

--- src/main/java/com/formationkilo/patientmvc/ui/PatientController.java
+++ src/main/java/com/formationkilo/patientmvc/ui/PatientController.java
@@ -5,9 +5,12 @@ import com.formationkilo.patientmvc.dao.PatientRepository;
 import com.formationkilo.patientmvc.dto.Patient;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,16 +20,25 @@ public class PatientController {
   // @Autowired
     private PatientRepository patientRepository;
 
-    @GetMapping(path = "/index")
+    @GetMapping(path = "/")
     public String patients(){
         return "patients";
     }
 
-  @GetMapping(path = "/")
+  @GetMapping(path = "/home")
   public String listPatients(Model model){
       List<Patient>listePatients = patientRepository.findAll();
       model.addAttribute("listePatients",listePatients);
     return "patients";
+  }
+  //the same method with pagination
+  @GetMapping(path = "/index")
+  public String paginationPatients(Model model,
+                                   @RequestParam(name="page", defaultValue = "0") int page,
+                                   @RequestParam(name="size", defaultValue = "5") int size){
+      Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+      model.addAttribute("listePatients",pagePatients.getContent());
+      return "patients";
   }
 
 }

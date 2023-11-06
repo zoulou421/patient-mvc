b6e2acad840d7f24f5fdf42bc10d2fedf21b0acd
Bonevy BEBY <bonevybeby@gmail.com>
2023-11-06 10:49:34 +0100

    add [Spring Boot Starter Validation] MAVEN DEPENDENCY for ENTITY VALIDATION: works well

--- src/main/java/com/formationkilo/patientmvc/dto/Patient.java
+++ src/main/java/com/formationkilo/patientmvc/dto/Patient.java
@@ -1,6 +1,9 @@
 package com.formationkilo.patientmvc.dto;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +16,15 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 2, max = 50)
     private  String nom;
 
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
-
     private boolean malade;
+    @DecimalMin("5")
     private int score;
 
 }

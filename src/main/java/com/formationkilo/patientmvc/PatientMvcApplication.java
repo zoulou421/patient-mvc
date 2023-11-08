26b2e08dcf11f7b80733e2bb67f2c25a39e94797
Bonevy BEBY <bonevybeby@gmail.com>
2023-11-08 13:32:23 +0100

    add PasswordEncoder to encrypt password -works successfully

--- src/main/java/com/formationkilo/patientmvc/PatientMvcApplication.java
+++ src/main/java/com/formationkilo/patientmvc/PatientMvcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -31,5 +33,9 @@ public class PatientMvcApplication {
             });
 
         };
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/formationkilo/patientmvc/sec/SecurityConfig.java
+++ src/main/java/com/formationkilo/patientmvc/sec/SecurityConfig.java
@@ -1,29 +1,40 @@
 package com.formationkilo.patientmvc.sec;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
 
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
        return
               new InMemoryUserDetailsManager(
+                    /*
                       User.withUsername("user1").password("{noop}1234").roles("USER").build(),
                       User.withUsername("user2").password("{noop}1234").roles("USER").build(),
                       User.withUsername("admin").password("{noop}1234").roles("USER","ADMIN").build()
+                     */
+                      User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                      User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                      User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+
               );
+
     }
 
 
